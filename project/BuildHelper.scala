28198b863fe481c79bdf276315fb70f40bd78755
Brandon Barker <brabarker@williamhill.us>
2021-12-08 19:02:08 -0500

    removing unsupported scala versions

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -254,7 +254,7 @@ object BuildHelper {
 
   def stdSettings(prjName: String) = Seq(
     name                                   := s"$prjName",
-    crossScalaVersions                     := Seq(Scala211, Scala212, Scala213),
+    crossScalaVersions                     := Seq(Scala213),
     ThisBuild / scalaVersion               := Scala213,
     scalacOptions                          := stdOptions ++ extraOptions(
       scalaVersion.value,
